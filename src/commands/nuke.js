e7b5f18702330a8e64354ab3dc5b7b99868d1d44
Velpy <iminspace5@gmail.com>
2025-11-27 20:25:35 -0600

    add nuke command

---
+++ src/commands/nuke.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('nuke')
+        .setDescription('Completely wipes this channel by deleting and recreating it')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+
+    async execute(interaction) {
+        const channel = interaction.channel;
+
+        await interaction.reply({
+            content: 'Channel is being wiped...',
+            ephemeral: true
+        });
+
+        const newChannel = await channel.clone();
+        await channel.delete();
+
+        await newChannel.send('Channel has been wiped and recreated.');
+    }
+};
